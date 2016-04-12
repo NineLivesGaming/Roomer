d979c448ef47f92c2a7b94a69e6103765faed229
Astgen <alexastgen@gmail.com>
2016-04-12 18:04:20 -0400

    got stook
    watching tutorials. I'll finish it soon™

---
+++ Roomer/Assets/basicMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicMovement : MonoBehaviour {
+
+	float gravity = 0.1f;
+	float jumpSpeed = 4f;
+
+	float walkDecay = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
