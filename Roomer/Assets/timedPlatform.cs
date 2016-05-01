5763d3cee56a1a134cdafbbadb27d2eeb0c65e8b
Astgen <alexastgen@gmail.com>
2016-05-01 18:41:20 -0400

    Disappearing floor
    It can easily be repurposed to a "fade" platform that will disappear
    when you step on it.

---
+++ Roomer/Assets/timedPlatform.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timedPlatform : MonoBehaviour {
+
+	public int timerOffset = 0;
+	public int timerSpeed = 10;
+	public int timerMax = 6000;
+
+	public int timerStayHidden = 2000;
+
+	public float scaleX, scaleY;
+
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		count = timerOffset;
+		scaleX = GetComponent<Transform>().localScale.x;
+		scaleY = GetComponent<Transform>().localScale.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((count -= timerSpeed) <= 0) {
+			count = timerMax + timerStayHidden;
+			GetComponent<BoxCollider2D> ().enabled = false;
+			GetComponent<Transform> ().localScale = new Vector3 (0,0,1);
+		} else {
+			if(count <= timerMax){
+				GetComponent<BoxCollider2D> ().enabled = true;
+			}
+		}
+
+		if(GetComponent<BoxCollider2D> ().enabled){
+			float newScaleX = scaleX * ((float)count / (float)timerMax);
+			newScaleX = (newScaleX > scaleX)?scaleX:newScaleX;
+
+			float newScaleY = scaleY * ((float)count / (float)timerMax);
+			newScaleY = (newScaleY > newScaleY)?scaleY:newScaleY;
+
+			GetComponent<Transform> ().localScale = new Vector3 (newScaleX,newScaleY,1);
+		}
+	}
+}
